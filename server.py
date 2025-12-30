595b8ff6579c0970685cb84325f518d6c06758c0
Ranesh Kumar <raneshku@gmail.com>
2025-12-30 04:44:41 -0500

    Task 8

--- server.py
+++ server.py
@@ -35,9 +35,12 @@ def sent_analyzer():
     # Check if the anger is None, indicating an error or invalid input
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        # Return a formatted string with the sentiment label and score
-        return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+
+    # Return a formatted string with the sentiment label and score
+    return f"For the given statement, \
+    the system response is 'anger': {anger}, \
+    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} \
+    and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
@@ -47,6 +50,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
